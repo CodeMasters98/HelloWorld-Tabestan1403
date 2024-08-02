a287d53cd8520a7cc21f6d80a3a69123cdbbe61f
Administrator <Administrator@PC-12>
2024-08-02 16:49:47 +0330

    Add project files.

---
+++ HelloWorld.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I am Parham!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldTopLevel/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//DataType Name = Default Value;
+
+string firstName;
+int age = 20;
+
+Console.WriteLine("Please enter your first name!");
+firstName = Console.ReadLine();
+//firstName = "Zahra";
+Console.WriteLine("You entered: " + firstName);
+Console.ReadLine();
