7795c82d7a05540d32cfee798025eca8720a7b05
Administrator <Administrator@PC-12>
2024-08-09 18:03:22 +0330

    change

--- HelloWorldTopLevel/Program.cs
+++ HelloWorldTopLevel/Program.cs
@@ -1,17 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+Console.WriteLine("Please enter your first name?");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name?");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Please enter your gender?(F: for female | M: For Male)");
+string gender = Console.ReadLine();
+
+Console.WriteLine("Please enter your age?");
+string ageStr = Console.ReadLine();
+
+int age = int.Parse(ageStr);
+//age condion
+
+bool isRegistered = false;
+
+if(age > 18 && gender == "M")
+    isRegistered = true;
+
+string yearGroup = default;
+
+if (age >= 0 && age <= 10)
+{
+    yearGroup = "Koodak";
+}
+else if( age >= 11 && age <= 19)
+{
+    yearGroup = "Nojavan";
+}
+else
+{
+    yearGroup = "Pir";
+}
+
+Console.WriteLine("Your firstName is " + firstName + " And ypur last name is " + lastName);
+Console.WriteLine("Your age is " + age + " -> " + yearGroup);
+
 //DataType Name = Default Value;
-string firstName;
-string FirstName;
+//string firstName = "";
+//string FirstName = null;
+//bool isActive = default;
+//int num3 = default;
 
-int age = 20;
-int newAge = 34;
-age = newAge * 2 + 1;
+//if (num3 == 0)
+//{
+//    Console.WriteLine("It is 0");
+//}
 
-Console.WriteLine(age);
-Console.WriteLine("Please enter your first name!");
-firstName = Console.ReadLine();
-//firstName = "Zahra";
-Console.WriteLine("You entered: " + firstName);
+//Console.WriteLine("Please enter your age?");
+//string ageStr = Console.ReadLine();
 
-Console.ReadLine();
+//var age = int.Parse(ageStr);
+//age = age + 1;
+
+
+//var name = "jashbd";
+
+//float num4 = 1.17F;
+
+//long num1 = 0;
+//int num2 = 1;
+//num2 = (int)num1;
+//num1 = num2;
+
+//int age = ageStr + 1;
