692075c626889340d6895b87480c0abd709b2f29
Administrator <Administrator@PC-12>
2024-08-23 17:20:51 +0330

    Add changes

--- HelloWorldTopLevel/Program.cs
+++ HelloWorldTopLevel/Program.cs
@@ -1,25 +1,152 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+//مدیریت حلقه
+//متد
+//سورس کد
+
+
+
+
+int count = 0;
+while (count < 5)
+{
+    var j = count * 2;
+    if (count == 3)
+    {
+        Console.WriteLine("You successfully reach number 3");
+        break;
+    }
+    Console.WriteLine(j);
+}
+
+// i = i + 1 => i++
+for (int i = 0; i < 5; i++)
+{
+    var j = i * 2;
+    if (i == 3)
+    {
+        Console.WriteLine("You successfully reach number 3");
+        //break;
+        continue;
+    }   
+    Console.WriteLine(j);
+}
+
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+
 Console.WriteLine("Please enter your first name?");
-string firstName = Console.ReadLine();
+string firstName = Console.ReadLine() ?? string.Empty; //null handle
+firstName = firstName.Trim();
+
+if (string.IsNullOrEmpty(firstName))
+{
+    Console.WriteLine("Invalid FirstName!");
+    Console.ReadLine();
+    return;
+}
+
+//به دست آوردن تعداد کاراکتر یک رشته
+int lenFirstName = firstName.Length;
+//تبدیل به کاراکترهای بزرگ
+firstName = firstName.ToUpper();
+//تبدیل بخ کارکاتر های کوچک
+firstName = firstName.ToLower();
+//حذف فاصله ها
+firstName = firstName.Trim();
+//بخشی از یک رشته را جدا کنم.
+var subFirstName = firstName.Substring(0,3);
+//startwith
+//endwith
+//array
+//show string $""
+//Next Line into string
+//replace
+//Get Type in C#
 
 Console.WriteLine("Please enter your last name?");
-string lastName = Console.ReadLine();
+string lastName = Console.ReadLine() ?? string.Empty;
+lastName = lastName.Trim();
 
-Console.WriteLine("Please enter your gender?(F: for female | M: For Male)");
-string gender = Console.ReadLine();
+Console.WriteLine("Please enter your phone?"); //09129564205
+string phone = Console.ReadLine() ?? string.Empty;
+phone = phone.Trim();
+if (phone.StartsWith("+98"))
+{
+    phone = phone.Replace("+98", "0");//11
+}
+if (phone.StartsWith("098"))
+{
+    phone = phone.Replace("098", "0");
+}
+if(phone.Length == 10 && !phone.StartsWith("0"))
+{
+    phone = "0" + phone;
+}
 
-Console.WriteLine("Please enter your age?");
-string ageStr = Console.ReadLine();
+var item = phone.ToArray();
+Console.WriteLine(item.GetType());
 
-int age = int.Parse(ageStr);
-//age condion
+//One way
+//string subPhone = phone.Substring(0, 2);
+//if (subPhone != "09")
+//{
+//    Console.WriteLine("You entered invalid phone");
+//}
+//another way
+if (!phone.StartsWith("09"))
+{
+    Console.WriteLine("You entered invalid phone");
+}
+
+int lenPhone = phone.Length;
+if (lenPhone != 11)
+{
+    Console.WriteLine("You entered invalid phone");
+}
+
+
+Console.WriteLine("Please enter your gender?(F: for female | M: For Male | N: Not say)"); //"M    "    "m" -> "M", "parham" -> "PARHAM"
+string gender = Console.ReadLine() ?? string.Empty;
+gender = gender.ToUpper();
 
 bool isRegistered = false;
 
-if(age > 18 && gender == "M")
-    isRegistered = true;
+switch (gender)
+{
+    case "M":
+        Console.WriteLine("You are Male");
+        isRegistered = true;
+        break;
+    case "F":
+        Console.WriteLine("You are FeMale");
+        isRegistered = true;
+        break;
+    case "N":
+        Console.WriteLine("You did not say your gender");
+        isRegistered = false;
+        break;
+    default:
+        Console.WriteLine("Please enter valid option");
+        isRegistered = false;
+        break;
+}
+
+Console.WriteLine("Please enter your age?");
+string ageStr = Console.ReadLine() ?? string.Empty; //"25 "
+ageStr = ageStr.Trim();
+
+int age = int.Parse(ageStr); // "25" + 1 -> 25 + 1
+//age condion
+
+//bool isRegistered = false;
+
+//if(age > 18 && gender == "M")
+//    isRegistered = true;
 
 string yearGroup = default;
 
@@ -35,9 +162,15 @@ else
 {
     yearGroup = "Pir";
 }
+//string description = "Your firstName is " + firstName + " And ypur last name is " + lastName;
+string description = $"Your firstName is {firstName} And your last name is {lastName}";
+Console.WriteLine(description);
+Console.WriteLine($"Your age is {age} -> {yearGroup}");
 
-Console.WriteLine("Your firstName is " + firstName + " And ypur last name is " + lastName);
-Console.WriteLine("Your age is " + age + " -> " + yearGroup);
+string description4 = $"Your firstName is  And your last name is \n" +
+    $"This test";
+Console.WriteLine(description4);
+
 
 //DataType Name = Default Value;
 //string firstName = "";
---
+++ UserData/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Xml.Linq;
+
+namespace UserData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //FirstName
+                Console.WriteLine(GetMessage("first name"));
+                string name = GetStringDataFromUser();
+                if (IsExit(name))
+                {
+                    PrintExit();
+                    break;
+                }
+
+                //LastName
+                Console.WriteLine(GetMessage("last name"));
+                string lastName = GetStringDataFromUser();
+                if (IsExit(lastName))
+                {
+                    PrintExit();
+                    break;
+                }
+                PrintSeperator();
+
+                //Age
+                Console.WriteLine(GetMessage("age"));
+                int age = GetNumericDataFromUser();
+                PrintSeperator();
+
+                //PhoneNumber
+                Console.WriteLine(GetMessage("phone number"));
+                string phoneNumber = GetStringDataFromUser();
+                if (IsExit(phoneNumber))
+                {
+                    PrintExit();
+                    break;
+                }
+                PrintSeperator();
+
+                //Show Profile
+                PrintProfile(firstName: name,lastName: lastName,age:  age, phoneNumber: phoneNumber);
+            }
+        }
+
+        static void PrintProfile(string firstName,string lastName, int age,string phoneNumber)
+        {
+            PrintUserOutput("firstName", firstName);
+            PrintUserOutput("lastName", lastName);
+            PrintUserOutput("age", age.ToString());
+            PrintUserOutput("phoneNumber", phoneNumber);
+        }
+
+        static bool IsExit(string str)
+        {
+            if (str == "E" || str == "e")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Single line method;
+        static string GetMessage(string property)
+            => $"Please enter your {property}?(If you want to exit please enter -> **E**)";
+
+        static void PrintUserOutput(string property, string str)
+        {
+            Console.WriteLine($"You enter {property}: {str} !!");
+        }
+
+        static void PrintSeperator()
+        {
+            Console.WriteLine($"****************Thank you*******************");
+        }
+
+        static void PrintExit()
+        {
+            Console.WriteLine("You exits from app!");
+        }
+
+        static string GetStringDataFromUser()
+        {
+            string userInput = Console.ReadLine() ?? "";
+            return userInput.Trim();
+        }
+
+        static int GetNumericDataFromUser()
+        {
+            string userInput = GetStringDataFromUser();
+            return Convert.ToInt32(userInput);
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static void PrintSum(int num1, int num2)
+        {
+            Console.WriteLine($"{Sum(num1, num2)}");
+        }
+
+        static int Sum2(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
